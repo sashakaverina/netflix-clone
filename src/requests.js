e578c4f40ecfb838f552afada15cbd24511c1096
Sasha Kaverina <sasha.kaverina@lewagon.org>
2021-10-04 23:35:47 +0900

    api-key done

---
+++ src/requests.js
@@ -0,0 +1,16 @@
+const API_KEY = "19f84e11932abbc79e6d83f82d6d1045";
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${APIKEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/discover/movie?api_key=${API_KEY}&language=en-US`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchMystery: `/discover/movie?api_key=${API_KEY}&with_genres=9648`,
+    fetchSciFi: `/discover/movie?api_key=${API_KEY}&with_genres=878`,
+    fetchWestern: `/discover/movie?api_key=${API_KEY}&with_genres=37`,
+    fetchAnimation: `/discover/movie?api_key=${API_KEY}&with_genres=16`,
+}
+
+export default requests;
