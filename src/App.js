4603b51bfcf65ff1d7d2e801daff9bb0dc461180
Sasha Kaverina <sasha.kaverina@lewagon.org>
2021-10-05 23:11:35 +0900

    banner

--- src/App.js
+++ src/App.js
@@ -2,12 +2,25 @@ import React from 'react';
 import './App.css';
 import Row from './Row';
 import requests from './requests';
+import Banner from './Banner';
 
 function App() {
   return (
     <div className="App">
-    <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
-    <Row title="trending now" fetchUrl={requests.fetchTrending}/>
+    <Banner />
+    <Row title="NETFLIX ORIGINALS" 
+         fetchUrl={requests.fetchNetflixOriginals}
+         isLargeRow/>
+
+    <Row title="Trending now" fetchUrl={requests.fetchTrending}/>
+    <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
+    <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
+    <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
+    <Row title="Animation" fetchUrl={requests.fetchAnimation}/>
+    <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
+    <Row title="Mystery" fetchUrl={requests.fetchMystery}/>
+    <Row title="Mystery" fetchUrl={requests.fetchMystery}/>
+    <Row title="Western" fetchUrl={requests.fetchWestern}/>
     </div>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -2,10 +2,10 @@ import React, {useState, useEffect} from 'react';
 import axios from './axios';
 import './Row.css';
 
-const base_url = "https://image.tmdb.org/t/p/w200";
+const base_url = "https://image.tmdb.org/t/p/w500";
 
 
-function Row( { title, fetchUrl } ) {
+function Row( { title, fetchUrl, isLargeRow } ) {
     const [movies, setMovies ] = useState([]);
 
     useEffect(() => {
@@ -26,8 +26,9 @@ function Row( { title, fetchUrl } ) {
         <div className="row__posters">
             {movies.map(movie  => (
                 <img 
-                className="row__poster"
-                src={`${base_url}${movie.poster_path}`} alt={movie.name}/>
+                key={movie.id}
+                className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path }`} alt={movie.name}/>
             ))}
 
         </div>
