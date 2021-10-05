906c1ff043e1a626083ea7fa8584ad3d715ef8ca
Sasha Kaverina <sasha.kaverina@lewagon.org>
2021-10-05 23:33:54 +0900

    banner styling done

--- src/Banner.js
+++ src/Banner.js
@@ -42,9 +42,12 @@ function Banner() {
                       <button className="banner__button">My List</button>
                   </div>
                   <h1 className="banner__description">{truncate(movie?.overview, 150)}</h1>
-
+    
                   
               </div>  
+              <div className="banner__fadeBottom">
+
+              </div>
                 
             </header>
         )
