766d6adc94a40f83696471df667e254e59a2d1f4
Sasha Kaverina <sasha.kaverina@lewagon.org>
2021-10-05 23:20:00 +0900

    banner styling

--- src/Banner.js
+++ src/Banner.js
@@ -19,6 +19,11 @@ function Banner() {
       }
       fetchData();
     }, []);
+
+
+    function truncate(str, n) {
+        return str?.length > n ? str.substr(0, n - 1) + "..." :str;
+    }
     
 
         return (
@@ -36,7 +41,7 @@ function Banner() {
                       <button className="banner__button">Play</button>
                       <button className="banner__button">My List</button>
                   </div>
-                  <h1 className="banner__description">{movie?.overview}</h1>
+                  <h1 className="banner__description">{truncate(movie?.overview, 150)}</h1>
 
                   
               </div>  
