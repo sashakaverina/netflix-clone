73ed38af03b4b4fe78eef5fd3cf9cb22eca45364
Sasha Kaverina <sasha.kaverina@lewagon.org>
2021-10-05 21:00:15 +0900

    row vertically

--- src/Row.js
+++ src/Row.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import axios from './axios';
+import './Row.css';
 
 const base_url = "https://image.tmdb.org/t/p/w200";
 
@@ -24,10 +25,14 @@ function Row( { title, fetchUrl } ) {
         
         <div className="row__posters">
             {movies.map(movie  => (
-                <img src={`${base_url}${movie.poster_path}`} alt={movie.name}/>
+                <img 
+                className="row__poster"
+                src={`${base_url}${movie.poster_path}`} alt={movie.name}/>
             ))}
 
         </div>
+
+   
 
       </div>
     );
